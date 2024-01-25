3b8207e32075cf83e11e017c27a37790574296b7
ottagit <ottaosawa@gmail.com>
2024-01-25 16:00:39 +0300

    include additional permissions in policy

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 # Deploy in CircleCI workflow
 module "github_actions" {
-  source = "github.com/ottagit/modules//ci-cd/global/iam/github-actions?ref=v0.2.1"
+  source = "github.com/ottagit/modules//ci-cd/global/iam/github-actions?ref=v0.2.2"
 
   allowed_repos_branches = [{
     org    = "ottagit"
