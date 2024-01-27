c3ed9fc5c89009565eecdbf8a8c4cde200d694a4
ottagit <ottaosawa@gmail.com>
2024-01-27 19:27:15 +0300

    grant github actions role full access to resource

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 # Deploy in CircleCI workflow
 module "github_actions" {
-  source = "github.com/ottagit/modules//ci-cd/global/iam/github-actions?ref=v0.2.3"
+  source = "github.com/ottagit/modules//ci-cd/global/iam/github-actions?ref=v0.2.4"
 
   allowed_repos_branches = [{
     org    = "ottagit"
