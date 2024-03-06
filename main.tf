e3cc81103491dd87f58eb25091f6a054359e93d0
ottagit <ottaosawa@gmail.com>
2024-03-06 10:36:29 +0300

    remove malformed statement ID

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 # Deploy in CircleCI workflow
 module "github_actions" {
-  source = "github.com/ottagit/modules//ci-cd/global/iam/github-actions?ref=v0.4.2"
+  source = "github.com/ottagit/modules//ci-cd/global/iam/github-actions?ref=v0.4.3"
 
   allowed_repos_branches = [{
     org    = "ottagit"
